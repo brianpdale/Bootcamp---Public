eb7c6bbf90ded9225fa1055dc7bfd48d0bc7dd94
brianpdale <82331979+brianpdale@users.noreply.github.com>
2021-04-23 19:44:26 -0700

    commit

---
+++ Fizzbuzz/Fizzbuzz.js
@@ -0,0 +1,17 @@
+var Array = []
+var x = 0
+;{
+for(var i=1; i<100; i++)
+    if(i % 3 === 0 && i % 5 === 0)
+        Array.push('FizzBuzz')
+    else
+    if(i % 3 === 0)
+    Array.push("Fizz");
+    else
+    if(i % 5 === 0)
+    Array.push("Buzz")
+    else
+    Array.push(i)}
+
+    console.log(Array)
+    
