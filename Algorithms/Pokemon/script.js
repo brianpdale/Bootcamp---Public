cccf8b9ff1e1838668ab5be028dd49e6b2ed8b56
brianpdale <82331979+brianpdale@users.noreply.github.com>
2021-04-27 12:51:32 -0700

    commit

---
+++ Algorithms/Pokemon/script.js
@@ -0,0 +1,56 @@
+var pokémon = [
+    { "id": 1,   "name": "Bulbasaur",  "types": ["poison", "grass"] },
+    { "id": 5,   "name": "Charmeleon", "types": ["fire"] },
+    { "id": 9,   "name": "Blastoise",  "types": ["water"] },
+    { "id": 12,  "name": "Butterfree", "types": ["bug", "flying"] },
+    { "id": 16,  "name": "Pidgey",     "types": ["normal", "flying"] },
+    { "id": 23,  "name": "Ekans",      "types": ["poison"] },
+    { "id": 24,  "name": "Arbok",      "types": ["poison"] },
+    { "id": 25,  "name": "Pikachu",    "types": ["electric"] },
+    { "id": 37,  "name": "Vulpix",     "types": ["fire"] },
+    { "id": 52,  "name": "Meowth",     "types": ["normal"] },
+    { "id": 63,  "name": "Abra",       "types": ["psychic"] },
+    { "id": 67,  "name": "Machamp",    "types": ["fighting"] },
+    { "id": 72,  "name": "Tentacool",  "types": ["water", "poison"] },
+    { "id": 74,  "name": "Geodude",    "types": ["rock", "ground"] },
+    { "id": 87,  "name": "Dewgong",    "types": ["water", "ice"] },
+    { "id": 98,  "name": "Krabby",     "types": ["water"] },
+    { "id": 115, "name": "Kangaskhan", "types": ["normal"] },
+    { "id": 122, "name": "Mr. Mime",   "types": ["psychic"] },
+    { "id": 133, "name": "Eevee",      "types": ["normal"] },
+    { "id": 144, "name": "Articuno",   "types": ["ice", "flying"] },
+    { "id": 145, "name": "Zapdos",     "types": ["electric", "flying"] },
+    { "id": 146, "name": "Moltres",    "types": ["fire", "flying"] },
+    { "id": 148, "name": "Dragonair",  "types": ["dragon"] }
+];
+
+var monster = {
+    id: 1,
+    name: "Bulbasaur",
+    types: ["poison", "grass"]
+};
+
+var pokemongreaterthan3 = []
+for(var i=0; i<pokémon.length; i++) {
+    if(pokémon[i].id % 3 == 0)
+        pokemongreaterthan3.push(pokémon[i])
+    }
+    console.log(pokemongreaterthan3);
+    
+    
+    var pokemonwithmorethanonetype = []
+    for(var i=0; i<pokémon.length; i++) {
+        if(pokémon[i].types.length > 1)
+            pokemonwithmorethanonetype.push(pokémon[i])
+    }
+    console.log(pokemonwithmorethanonetype);
+    
+    var pokemonwithonlypoison = []
+    for(var i=0; i<pokémon.length; i++)
+        if(pokémon[i].types.length == 1 && pokémon[i].types == "poison")
+
+            pokemonwithonlypoison.push(pokémon[i])
+        
+    console.log(pokemonwithonlypoison);
+
+
