b04dba529bdb94db359a563a94bf58487ce94eba
brianpdale <82331979+brianpdale@users.noreply.github.com>
2021-04-27 16:36:57 -0700

    commit

--- Profile Page/script.js
+++ Profile Page/script.js
@@ -20,3 +20,34 @@ function counter(){
     num ++;
     document.getElementById("counting").innerHTML = num;
 }
+var username = document.querySelector(".brian");
+function changeprofilename(){
+    document.getElementById("brian").innerHTML = "Jane Doe";
+}
+var requests = document.getElementById("numrequests").innerHTML;
+var connections = document.getElementById("numconnections").innerHTML;
+
+function accepttodde(){
+    document.getElementById("todde").style.display = 'none';
+    requests--;
+    connections++;
+    document.getElementById("numrequests").innerHTML = requests;
+    document.getElementById("numconnections").innerHTML = connections;
+}
+function acceptphilk(){
+        document.getElementById("philk").style.display = 'none';
+        requests--;
+        connections++;
+        document.getElementById("numrequests").innerHTML = requests;
+        document.getElementById("numconnections").innerHTML = connections;
+}
+function closetodde(){
+    document.getElementById("todde").style.display = 'none';
+    requests--;
+    document.getElementById("numrequests").innerHTML = requests;
+}
+function closephilk(){
+        document.getElementById("philk").style.display = 'none';
+        requests--;
+        document.getElementById("numrequests").innerHTML = requests;
+}
