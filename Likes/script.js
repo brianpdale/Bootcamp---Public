d58fd11dff91b22883f36aea5ba79a6b1e6f8fe3
brianpdale <82331979+brianpdale@users.noreply.github.com>
2021-04-27 14:21:05 -0700

    commit

--- Likes/script.js
+++ Likes/script.js
@@ -1,6 +1,17 @@
-function count(){
-        var num = document.getElementById("num").innerHTML;
+function count1(){
+        var num = document.getElementById("num1").innerHTML;
         num ++;
-        document.getElementById("num").innerHTML = num;
+        document.getElementById("num1").innerHTML = num;
         console.log(num)
+}
+function count2(){
+        var num = document.getElementById("num2").innerHTML;
+        num ++;
+        document.getElementById("num2").innerHTML = num;
+        console.log(num1)
+}function count3(){
+        var num = document.getElementById("num3").innerHTML;
+        num ++;
+        document.getElementById("num3").innerHTML = num;
+        console.log(num2)
 }
