a681d03d1951859bdb43ee3d5caf5a6cd02bda56
brianpdale <82331979+brianpdale@users.noreply.github.com>
2021-04-28 14:23:29 -0700

    commit

---
+++ Yellow Belt Exam/script.js
@@ -0,0 +1,13 @@
+
+    function acceptcookies(){
+        document.getElementById("cookies").style.display = 'none';
+}
+
+function changepic(){
+    var myelement = document.querySelector("#succulent1")
+    myelement.src = "succulents-1.jpg"
+}
+function changepicback(){
+    var myelement = document.querySelector("#succulent1")
+    myelement.src = ""
+}
