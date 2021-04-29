da65b0886b5245c9b0b8c6913041f7701c32405b
brianpdale <82331979+brianpdale@users.noreply.github.com>
2021-04-28 18:43:11 -0700

    commit

---
+++ Dojo Weather/script.js
@@ -0,0 +1,9 @@
+function scheduleA(event) {
+        alert(this.options[this.selectedIndex].text);
+}
+function addShadow(element){
+    element.classList.add ("shadow");
+}
+function removeShadow(element){
+    element.classList.remove ("shadow");
+}
