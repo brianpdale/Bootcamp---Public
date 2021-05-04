cfdd03c61e4263efb7138c22f698ce0e5e5cc968
brianpdale <82331979+brianpdale@users.noreply.github.com>
2021-05-03 17:56:53 -0700

    commit

--- Function_Basics_2.py
+++ Function_Basics_2.py
@@ -1,7 +1,35 @@
 
+def count_down(n):
+    counter = []
+    for x in range(n, -1, -1):
+        counter.append(x)
+    print(counter)  
+count_down(99)
 
-def countdown(startnum):
-    newnumbers = []
-    input(startnum)
-    return startnum
-    print(startnum)
+def printandreturn(a):
+    print(a[0])
+    return(a[1])
+printandreturn([5,6])
+
+def first_plus_length(a):
+    sumoffandl = ((a[0]) + len(a))
+    print(sumoffandl)
+first_plus_length([1,2,3,4,5,6])
+
+def values_greater_than_second(a):
+    greaterthan = []
+    for x in range(len(a)):
+        if a[x] > a[1]:
+            greaterthan.append(a[x])
+    print(len(greaterthan))
+    print(greaterthan)
+    print(len(greaterthan) >= 2)
+values_greater_than_second([3,5,1,8,2])
+
+
+def this_length_that_value(size, value):
+    givensize = []
+    for x in range(size):
+        givensize.append(value)
+    print(givensize)
+this_length_that_value(9,1)
