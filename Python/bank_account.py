456b142ec45f83206dcd5cb1e5289d99b9bdf8e8
brianpdale <82331979+brianpdale@users.noreply.github.com>
2021-05-05 15:07:10 -0700

    commit

--- Python/bank_account.py
+++ Python/bank_account.py
@@ -1,20 +1,21 @@
 class BankAccount:
-    def __init__(self, int_rate, balance): 
-        default_int_rate = .01
-        default_balance = 0
-        self.int_rate = .01
-        self.balance = balance if balance is None else default_balance
-
+    def __init__(self, balance = 0, int_rate = .01): 
+        # default_int_rate = .01
+        # default_balance = 0
+        # self.int_rate = .01
+        # self.balance = balance if balance is None else default_balance
+        self.int_rate = int_rate
+        self.balance = balance
     def deposit(self, amount):
         self.balance += amount
         return self
     def withdraw(self, amount):
         if self.balance > amount:
-            self.withdraw -= amount
+            self.balance -= amount
+            return self
         else:
             print("Insufficient funds: Charging a $5 fee")
             self.balance -= 5
-
         return self
     def display_account_info(self):
         print("Balance: $" + str(self.balance) + "  Interest Rate: %" + str(self.int_rate*100))
@@ -22,15 +23,12 @@ class BankAccount:
     def yield_interest(self):
         self.balance = (self.balance * self.int_rate) + self.balance
         return self
-
-account1 = BankAccount(.012, 100)
-account2 = BankAccount('' , '')
+        
+account1 = BankAccount(100)
+account2 = BankAccount()
 
 account1.display_account_info()
 account2.display_account_info()
 
-account1.deposit(100).yield_interest()
-print(account1.balance)
-account1.display_account_info()
-account1.withdraw(300)
-print(account1.balance)
+account1.deposit(100).deposit(200).deposit(600).withdraw(400).yield_interest().display_account_info()
+account2.deposit(200).deposit(400).withdraw(200).withdraw(300).withdraw(100).withdraw(100).yield_interest().display_account_info()
